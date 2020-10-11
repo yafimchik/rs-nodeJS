e364b7bee327bddf1a20822b1585eeca3e9f7eeb
Yauheni <yafimenka.jk@gmail.com>
2020-10-12 00:15:24 +0300

    fix: fix inheritance of services

--- src/resources/tasks/task.service.js
+++ src/resources/tasks/task.service.js
@@ -23,6 +23,15 @@ class TasksService extends PrototypeService {
     return result;
   }
 
+  async update(obj) {
+    const entity = await this.getById(obj.boardId, obj.id);
+    if (!entity) return null;
+
+    const newEntity = new this.model(obj);
+    const result = await this.repo.put(newEntity);
+    return result;
+  }
+
   async deleteAllBoardTasks(boardId) {
     const boardTasks = await this.getAllByBoardId(boardId);
     if (!boardTasks.length) {
