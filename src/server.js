7ad50285f087e48d1ec1fdcd15c8d64ba6503eae
Yauheni <yafimenka.jk@gmail.com>
2020-10-15 10:36:09 +0300

    feat: add handling od exceptions & rejections

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 const { PORT } = require('./common/config');
 const app = require('./app');
+const logger = require('./common/logger');
+
+process.on('uncaughtException', logger.addUncaughtException.bind(logger));
+
+process.on('unhandledRejection', logger.addUnhandledRejection.bind(logger));
 
 app.listen(PORT, () =>
   console.log(`App is running on http://localhost:${PORT}`)
