53d8dd5f1defe6c3cf4384d9ce285703ff64595d
Yauheni <yafimenka.jk@gmail.com>
2020-10-16 21:20:06 +0300

    fix: fix init of mongo db

--- src/server.js
+++ src/server.js
@@ -8,11 +8,17 @@ process.on('unhandledRejection', logger.addUnhandledRejection.bind(logger));
 const app = require('./app');
 
 async function startApp() {
-  await mongoose.connect(MONGO_CONNECTION_STRING, { useNewUrlParser: true });
-
+  logger.addStatus('ready to connect to DB');
+  await mongoose.connect(MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  });
+  logger.addStatus('DB connected');
   app.listen(PORT, () =>
     console.log(`App is running on http://localhost:${PORT}`)
   );
+  logger.addStatus('Server is running');
 }
 
 startApp();
