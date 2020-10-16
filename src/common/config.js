117d73fc5f2736299c9509951dc7596207a33122
Yauheni <yafimenka.jk@gmail.com>
2020-10-16 21:14:32 +0300

    feat: add output logger

--- src/common/config.js
+++ src/common/config.js
@@ -11,6 +11,7 @@ const LOG_MODES = {
 };
 
 const LOG_FILE_PATH = path.resolve(__dirname, '../', process.env.LOG_FILE_PATH);
+console.log(LOG_FILE_PATH);
 
 module.exports = {
   PORT: process.env.PORT,
