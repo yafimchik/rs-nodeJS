f3c0829e561efee76d1f22afedaae57efe275fc0
Yauheni <yafimenka.jk@gmail.com>
2020-10-16 21:19:42 +0300

    feat: add db config to column model

--- src/resources/boards/column.model.js
+++ src/resources/boards/column.model.js
@@ -1,5 +1,11 @@
-class Column {
-  constructor({ title = 'title', order = 1 } = {}) {
+const Model = require('../../common/prototype.model');
+const { v4: uuid } = require('uuid');
+
+class Column extends Model {
+  constructor(obj = {}) {
+    super(obj);
+    const { id = uuid(), title = 'title', order = 1 } = obj;
+    this.id = id;
     this.title = title;
     this.order = order;
   }
@@ -8,18 +14,26 @@ class Column {
     return 'Column';
   }
 
-  static toType() {
+  static toSchemaType() {
     const type = {
+      id: {
+        type: String,
+        required: true
+      },
       title: {
         type: String,
         required: true
       },
       order: {
-        type: String,
+        type: Number,
         required: true
       }
     };
     return type;
+  }
+
+  static toPropsArray() {
+    return ['id', 'title', 'order'];
   }
 }
 
