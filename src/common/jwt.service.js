ae89d782eb9f2e460fa30882ee4f7cfdec564b8c
Yauheni <yafimenka.jk@gmail.com>
2020-10-20 11:56:32 +0300

    feat: realize jwt service

--- src/common/jwt.service.js
+++ src/common/jwt.service.js
@@ -1,24 +1,24 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET_KEY } = require('./config');
+
 class JwtService {
-  constructor() {
-    // TODO jwt package
+  constructor(jwtLib, secret) {
+    this.jwt = jwtLib;
+    this.secret = secret;
   }
 
-  generateJWT(user) {
-    // TODO jwt gen
-    return user;
+  async generateJWT(user) {
+    const payload = { userId: user.id, login: user.login };
+    const token = await this.jwt.sign(payload, this.secret);
+    return token;
   }
 
-  verifyJWT(jwt) {
-    // TODO check jwt;
-    return !!jwt;
-  }
-
-  getUserFromJWT(jwt) {
-    // TODO parse jwt
-    return jwt;
+  async verifyJWT(token) {
+    const result = await this.jwt.verify(token, this.secret);
+    return result;
   }
 }
 
-const jwtService = new JwtService();
+const jwtService = new JwtService(jwt, JWT_SECRET_KEY);
 
 module.exports = jwtService;
