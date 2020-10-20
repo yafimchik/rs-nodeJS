393495c48f60aa177cf3a8cb130e9418c29c83e6
Yauheni <yafimenka.jk@gmail.com>
2020-10-20 11:59:12 +0300

    feat: realize authentication service

--- src/middlewares/verify-token.middleware.js
+++ src/middlewares/verify-token.middleware.js
@@ -1,13 +1,20 @@
+const { AUTH_MODE } = require('../common/config');
 const jwtService = require('../common/jwt.service');
 
-function verifyToken(req, res, next) {
+async function verifyToken(req, res, next) {
+  if (!AUTH_MODE) {
+    next();
+    return;
+  }
   const bearerHeader = req.headers.authorization;
 
   if (bearerHeader) {
     const bearer = bearerHeader.split(' ');
     const bearerToken = bearer[1];
 
-    if (jwtService.verifyJWT(bearerToken)) {
+    const result = await jwtService.verifyJWT(bearerToken);
+
+    if (result) {
       next();
       return;
     }
