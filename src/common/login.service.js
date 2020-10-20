c6427f4b0c26134d5d36df6c52272bc2203c6257
Yauheni <yafimenka.jk@gmail.com>
2020-10-20 11:58:50 +0300

    feat:realize login service

--- src/common/login.service.js
+++ src/common/login.service.js
@@ -1,26 +1,37 @@
+const cryptService = require('./crypt.service');
+const jwtService = require('./jwt.service');
+const usersService = require('../resources/users/user.service');
+
 class LoginService {
   constructor(usersServ, jwtServ, cryptServ) {
     this.jwtService = jwtServ;
     this.usersService = usersServ;
     this.cryptService = cryptServ;
-    // TODO jwt
   }
 
   async login(login, pswd) {
-    if (!login || pswd) {
+    if (!login || !pswd) {
       return null;
     }
 
-    const user = await this.usersService.findUserByLogin(login);
-
+    const user = await this.usersService.getByLogin(login);
+    console.log({ login, pswd });
+    console.log(user);
     if (!user) return null;
 
-    if (this.cryptService.compareStringWithHash(pswd, user.password)) {
-      const jwt = this.jwtService.generateJWT(user);
+    const result = await this.cryptService.compareStringWithHash(
+      pswd,
+      user.password
+    );
+    if (result) {
+      const jwt = await this.jwtService.generateJWT(user);
+      console.log(jwt);
       return { token: jwt };
     }
     return null;
   }
 }
 
-module.exports = LoginService;
+const loginService = new LoginService(usersService, jwtService, cryptService);
+
+module.exports = loginService;
--- src/middlewares/login.middleware.js
+++ src/middlewares/login.middleware.js
@@ -1,14 +1,11 @@
 // const logger = require('../common/logger');
-const cryptService = require('../common/crypt.service');
-const jwtService = require('../common/jwt.service');
-const usersService = require('../resources/users/user.service');
 
-const LoginService = require('../common/login.service');
+const loginService = require('../common/login.service');
 
 const loginHandler = async (req, res) => {
   const { login, password } = req.body;
-  const loginService = new LoginService(usersService, jwtService, cryptService);
 
+  console.log('login middleware is running');
   const loginResult = await loginService.login(login, password);
   if (loginResult) {
     res.status(200);
